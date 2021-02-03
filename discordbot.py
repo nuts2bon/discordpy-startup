32b355933f151f0f8ae88119298361c80b47b811
nuts2bon <sswmgu@gmail.com>
2021-02-03 13:20:03 +0900

    update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -11,7 +11,7 @@ import ikaTools
 #sys.stdout = codecs.getwriter("utf-8")(sys.stdout)
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
-ikaT = ikaTools()
+ikaT = ikaTools.ikaTools.ikaTools()
 
 #json_open_wp = open('./weapon.json','r')
 #weapons=json.load(json_open_wp)
